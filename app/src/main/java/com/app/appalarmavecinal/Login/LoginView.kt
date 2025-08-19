6608ef9eee42e57f5086c786d594c24f5b04ae73
Raul <neskirbi@gmail.com>
2025-08-19 17:09:32 -0600

    first commit

---
+++ app/src/main/java/com/app/appalarmavecinal/Login/LoginView.kt
@@ -0,0 +1,21 @@
+package com.app.appalarmavecinal.Login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.appalarmavecinal.R
+
+class LoginView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
